e5afe4ab39321376ad1b9bc8b9d8410cd1d5fa46
drschwenk <schwgit2@gmail.com>
2017-06-02 14:46:56 -0700

    starting a notebook on connected components

--- record_notebook_py/reproduce_baseline.py
+++ record_notebook_py/reproduce_baseline.py
@@ -86,7 +86,11 @@ import requests
 import base64
 import PIL.Image as Image
 import cv2
+import editdistance
 from urllib.request import url2pathname
+
+# <codecell>
+
 import ai2.vision.utils as ai2vu
 
 # <codecell>
@@ -164,6 +168,17 @@ with open(data_path_1) as f:
 
 # <markdowncell>
 
+# B. Structural Evaluation To begin with, while working on the initial pieces of the
+# tool, we focused on evaluating structural components. This centered around the number of detected rows and columns. To accomplish this, we labeled the ground truth number of rows and columns for the Regents dataset, as well as a descriptor of the complexity of the image (three categories of difficulty). Comparing to these labels, following the switch from heuristic to classifier merging, we were successfully able to match the number of detected rows and columns for many of the images, including even some of the most complex category, which included non-table images. Overall, we were detecting 
+# 
+# the __correct number of columns for 89.5%__
+# 
+# the __correct number rows for 91.6%__
+# 
+# of rows correctly. With these results, we decided to switch and focus on a more robust metric of cell- level
+
+# <markdowncell>
+
 # ![Screen%20Shot%202017-05-31%20at%2010.16.52%20AM.png](attachment:Screen%20Shot%202017-05-31%20at%2010.16.52%20AM.png)
 
 # <markdowncell>
@@ -176,10 +191,11 @@ with open(data_path_1) as f:
 api_entry_point_local = 'http://127.0.0.1:5000/v1/tableparse'
 header = {'Content-Type': 'application/json'}
 
+
 def request_table_parse(image, rq_attr):
     request_data = {
         rq_attr: image.decode('utf-8'),
-        'border_table': 'True'
+        'border_table': 'False'
     }
     json_data = json.dumps(request_data)
     response = requests.post(api_entry_point_local, data=json_data, headers=header)
@@ -187,6 +203,7 @@ def request_table_parse(image, rq_attr):
     if json_response:
         response = json_response
     return response
+
 
 def parse_table_image(image_path):
     b64_encoded_image = base64.b64encode(open(image_path, 'rb').read())
@@ -199,44 +216,23 @@ image_base_path = '/Users/schwenk/wrk/tableparse/data/test_data/images'
 
 # <codecell>
 
-test_table = list(table_grountruth.values())[9]
+table_parser_results = {}
+
+# <markdowncell>
+
+# first run 0-5, 6, 
 
 # <codecell>
 
-img_path = os.path.join(image_base_path, test_table['imageName'])
+start_idx = 5
+end_idx = 6
 
 # <codecell>
 
-Image.open(img_path)
-
-# <codecell>
-
-test_gt_box = test_table['annotations'][0]
-
-# <codecell>
-
-test_gt_box
-
-# <codecell>
-
-test_response = parse_table_image(img_path)
-
-# <codecell>
-
-test_response.keys()
-
-# <codecell>
-
-build_detected_boxes = [item for sublist in list(test_response.values()) for item in sublist]
-detected_boxes = [item for sublist in build_detected_boxes for item in sublist]
-
-# <codecell>
-
-detected_boxes[0]
-
-# <codecell>
-
-test_gt_box
+for table_id, table_anno in list(table_grountruth.items())[start_idx:end_idx]:
+    print(table_anno['imageName'])
+    img_path = os.path.join(image_base_path, table_anno['imageName'])
+    table_parser_results[table_id] = parse_table_image(img_path)
 
 # <markdowncell>
 
@@ -245,21 +241,71 @@ test_gt_box
 # <codecell>
 
 def boxes_overlap(detected_box, gt_box, thresh=0.5):
+    
+    def area(box):
+        return (box[3] - box[1]) * (box[2] - box[0])
+
     detected_coords = [[detected_box['rectangle'][n]['x'], detected_box['rectangle'][n]['y']] for n in range(2)]
     detected_coords = [item for sublist in detected_coords for item in sublist]
     gt_coords = gt_box['rectangle']
     gt_coords = gt_coords[:2] + list(map(np.add, gt_coords[:2], gt_coords[2:]))
-
-    def area(box):
-        return (box[3] - box[1]) * (box[2] - box[0])
-
+    
     dx = min(detected_coords[2], gt_coords[2]) - max(detected_coords[0], gt_coords[0])
     dy = min(detected_coords[3], gt_coords[3]) - max(detected_coords[1], gt_coords[1])
     if (dx >= 0) and (dy >= 0):
         intersection_area = dx * dy
-        return float(intersection_area) / min(area(detected_coords), area(gt_coords))
+        return float(intersection_area) / min(area(detected_coords), area(gt_coords)) > thresh
     else:
         return False
+    
+    
+def match_boxes(detected_boxes, gt_boxes, thresh=0.5):
+    matches = []
+    for db in detected_boxes:
+        for gtb in gt_boxes:
+            if boxes_overlap(db, gtb, thresh):
+                matches.append((gtb['text'], db['value']))
+    return matches
+
+
+def compute_eds(matched_cell_contents):
+    return [editdistance.eval(*cells) for cells in matched_cell_contents]
+
+
+def compute_ed_scores(parsed_tables, gt_tables):
+    ed_scores = {}
+    for table_id in parsed_tables:
+        detected_boxes = [item for sublist in parsed_tables[table_id]['columns'] for item in sublist]
+        gt_boxes = gt_tables[table_id]['annotations']
+        matched_cells = match_boxes(detected_boxes, gt_boxes, 0.5)
+        eds = compute_eds(matched_cells)
+        ed_scores[table_id] = eds
+    return ed_scores
+
+
+def compute_perf_table(error_distances):
+    ed_array = np.array([item for sublist in list(error_distances.values()) for item in sublist])
+    n_cells = ed_array.shape[0]
+    perc_perf_at_eds = [sum(ed_array < n) / n_cells  * 100 for n in list(range(1, 6)) + [13]]
+    perf_table = pd.DataFrame(perc_perf_at_eds).T
+    perf_table.columns = ['% @ 0 e.d', '% @ 1 e.d', '% @ 2 e.d', '% @ 3 e.d', '% @ 4 e.d', '% @ max e.d. (13)']
+    return perf_table
+
+# <codecell>
+
+error_dists = compute_ed_scores(table_parser_results, table_grountruth)
+
+# <markdowncell>
+
+# ## results
+
+# <codecell>
+
+compute_perf_table(error_dists)
+
+# <codecell>
+
+
 
 # <codecell>
 
@@ -288,3 +334,15 @@ def boxes_overlap(detected_box, gt_box, thresh=0.5):
 # <markdowncell>
 
 # * Next steps 1
+
+# <markdowncell>
+
+# ## hide
+
+# <codecell>
+
+# test_table = list(table_grountruth.values())[9]
+
+# img_path = os.path.join(image_base_path, test_table['imageName'])
+
+# test_response = parse_table_image(img_path)
